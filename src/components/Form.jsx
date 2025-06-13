ea53cf93c333528bc19aa7351f249125bb2a3510
Katya Weicht <katya.weicht@gmail.com>
2025-06-12 22:38:29 -0600

    Add save button to store data in local storage

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,21 @@
 import { useLocalStorage } from "../hooks/useLocalStorage";
 function Form() {
-  const [name, setName] = useLocalStorage("name", "")
-  const [serviceNumber, setServiceNumber] = useLocalStorage("serviceNumber", "")
+  const [name, setName] = useLocalStorage("name", "") 
+  const [serviceNumber, setServiceNumber] = useLocalStorage("serviceNumber", "") //use custom hook for input forms
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    alert("saved!")
+  }
+  
     return (
       <>
-        <form style={{ display: "flex", flexDirection: "column" }}>
+        <form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit}>
             <label htmlFor="name">Name:</label>
-            <input type="text" data-testid={"name"} />
+            <input type="text" data-testid={"name"} value={name} onChange={((event) => setName(event.target.value))}/>
             <label htmlFor="service">Service Number:</label>
-            <input type="text" data-testid={"service"} />
-
+            <input type="text" data-testid={"service"} value={serviceNumber} onChange={((event) => setServiceNumber(event.target.value))}/>
+            <button type="submit">Save</button>
         </form>
         <h4>{name ? `Welcome, ${name}!` : "Enter your name"}</h4>
       </>
